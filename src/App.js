0ac5cfca3a1cf4896885977bd23f9503d44ca5a7
Neeraja-Gadi <106535298+Neeraja-Gadi@users.noreply.github.com>
2023-07-23 15:49:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,70 @@
+
+import LandingPage from './components//HomePage/Landingpage'
+import UserProfile from './components/UserProfile.js'
+import Login from './components/loginPage/Login'
+import SignUp from './components/loginPage/SignUp'
+import EducationForm from './components/multiForm/EducationForm'
+import ProductSearch from "./components/Search.js"
+import Dashboard from './components/Search/SearchPreferences'
+import ForgotPassword from './components/loginPage/ForgotPassword'
+import DashboardPortfolio from './components/Portfolio/Portfolio'
+import ResetPassword from './components/loginPage/ResetPassword'
+import { Routes, Route } from 'react-router-dom'
+import UserProfileForm from './components/multiForm/UserProfileForm'
+import UserExperience from './components/multiForm/UserExperience'
+import ProjectForm from './components/multiForm/UserProjects'
+import Pricing from './components/revenueModal/SubscriptionModal'
+import JobPost from './components/RecruiterProfile/RecruiterProfileComponents/Forms/JobPost'
+import SearchResult from './components/Search/SearchComponents/SearchResult'
+import RecruiterProfilePage from './components/RecruiterProfile/RecruiterProfilePage'
+import TalentPoolNew from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/TalentPoolNew';
+import MyPlans from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/MyPlans'
+import Employer from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/Employer'
+import SearchedItems from './components/Search/SearchComponents/SearchedItems'
+import ProfileForm from "./components/RecruiterProfile/RecruiterProfileComponents/Forms/ProfileForm"
+// import DataTable from "./components/test"
+
+function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={
+                <>               
+                     <LandingPage />
+                </>}
+                 />
+                <Route path="Login" element={<Login />} />
+                <Route path="SignUp" element={<SignUp />} />
+                <Route path='ForgotPassword' element={<ForgotPassword />} />
+                <Route path='resetPassword/:id/:token' element={<ResetPassword />} />
+                <Route path='UserProfileForm' element={<UserProfileForm />} />
+                <Route path="UserProfile" element={<UserProfile />} />
+                <Route path="EducationForm" element={<EducationForm />} />
+                <Route path="ExperienceForm" element={<UserExperience />} />
+                <Route path="UserProjects" element={<ProjectForm />} />
+                <Route path="ProductSearch" element={<ProductSearch />} />
+                <Route path="JobSearch" element={<Dashboard />} />
+                <Route path='SearchResult' element={<SearchResult />} />
+                <Route path='SeacrhedItems' element={<SearchedItems />} />
+                <Route path='Portfolio' element={<DashboardPortfolio />} />
+                <Route path='SubscriptionModal' element={<Pricing />} />
+                <Route path='JobPost' element={<JobPost />} />            
+                <Route path='RecruiterProfilePage' element={<RecruiterProfilePage />} />
+                <Route path='MyPlans' element={<MyPlans />} />
+                <Route path='RecProfile' element={<ProfileForm />} />
+                {/* <Route path='Search' element={<DataTable />} />                 */}
+                <Route path="Employer/:uId/:id" element={<Employer />} />
+                <Route path='TalentPoolNew/:jid' element={<TalentPoolNew />} />
+               <Route path='ProfileForm' element={< ProfileForm/>} />            
+            </Routes>
+
+        </div>
+    )
+}
+export default App;
+
+
+
 // import HeroPage from './components/HomePage/HeroPage'
 // import Sec from './components/HomePage/Sec'
 // import Third from './components/HomePage/Third'
@@ -154,67 +221,3 @@
 // }
 // export default App;
 
-
-import LandingPage from './components//HomePage/Landingpage'
-import UserProfile from './components/UserProfile.js'
-import Login from './components/loginPage/Login'
-import SignUp from './components/loginPage/SignUp'
-import EducationForm from './components/multiForm/EducationForm'
-import ProductSearch from "./components/Search.js"
-import Dashboard from './components/Search/SearchPreferences'
-import ForgotPassword from './components/loginPage/ForgotPassword'
-import DashboardPortfolio from './components/Portfolio/Portfolio'
-import ResetPassword from './components/loginPage/ResetPassword'
-import { Routes, Route } from 'react-router-dom'
-import UserProfileForm from './components/multiForm/UserProfileForm'
-import UserExperience from './components/multiForm/UserExperience'
-import ProjectForm from './components/multiForm/UserProjects'
-import Pricing from './components/revenueModal/SubscriptionModal'
-import JobPost from './components/RecruiterProfile/RecruiterProfileComponents/Forms/JobPost'
-import SearchResult from './components/Search/SearchComponents/SearchResult'
-import RecruiterProfilePage from './components/RecruiterProfile/RecruiterProfilePage'
-import TalentPoolNew from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/TalentPoolNew';
-import MyPlans from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/MyPlans'
-import Employer from './components/RecruiterProfile/RecruiterProfileComponents/PageComponents/Employer'
-import SearchedItems from './components/Search/SearchComponents/SearchedItems'
-import ProfileForm from "./components/RecruiterProfile/RecruiterProfileComponents/Forms/ProfileForm"
-// import DataTable from "./components/test"
-
-function App() {
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={
-                <>               
-                     <LandingPage />
-                </>}
-                 />
-                <Route path="Login" element={<Login />} />
-                <Route path="SignUp" element={<SignUp />} />
-                <Route path='ForgotPassword' element={<ForgotPassword />} />
-                <Route path='resetPassword/:id/:token' element={<ResetPassword />} />
-                <Route path='UserProfileForm' element={<UserProfileForm />} />
-                <Route path="UserProfile" element={<UserProfile />} />
-                <Route path="EducationForm" element={<EducationForm />} />
-                <Route path="ExperienceForm" element={<UserExperience />} />
-                <Route path="UserProjects" element={<ProjectForm />} />
-                <Route path="ProductSearch" element={<ProductSearch />} />
-                <Route path="JobSearch" element={<Dashboard />} />
-                <Route path='SearchResult' element={<SearchResult />} />
-                <Route path='SeacrhedItems' element={<SearchedItems />} />
-                <Route path='Portfolio' element={<DashboardPortfolio />} />
-                <Route path='SubscriptionModal' element={<Pricing />} />
-                <Route path='JobPost' element={<JobPost />} />            
-                <Route path='RecruiterProfilePage' element={<RecruiterProfilePage />} />
-                <Route path='MyPlans' element={<MyPlans />} />
-                <Route path='RecProfile' element={<ProfileForm />} />
-                {/* <Route path='Search' element={<DataTable />} />                 */}
-                <Route path="Employer/:uId/:id" element={<Employer />} />
-                <Route path='TalentPoolNew/:jid' element={<TalentPoolNew />} />
-               <Route path='ProfileForm' element={< ProfileForm/>} />            
-            </Routes>
-
-        </div>
-    )
-}
-export default App;
