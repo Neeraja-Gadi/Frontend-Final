d6e120abcbc53bb5a407256b98edcf91f39c8fcc
Neeraja-Gadi <106535298+Neeraja-Gadi@users.noreply.github.com>
2023-07-23 15:45:21 +0530

    Update MyPlans.js

--- src/components/RecruiterProfile/RecruiterProfileComponents/PageComponents/MyPlans.js
+++ src/components/RecruiterProfile/RecruiterProfileComponents/PageComponents/MyPlans.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,7 +6,6 @@ import CardContent from '@mui/material/CardContent';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
